3db2755d1a16e40ba19f0b12e4e5e75a081f9866
Weerawut <weerawut@live.com>
2024-11-30 16:36:38 +0700

    Initial Commit

---
+++ AddingGame/AddingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingGameApp.swift
+//  AddingGame
+//
+//  Created by Weerawut Chaiyasomboon on 30/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct AddingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
